caaa904f1bad621d38d657a1ce8e8bae2d244de0
Akashshripal <akashshripal786@gmail.com>
2023-07-05 10:53:42 +0530

    initial update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 import Navbar from './Components/Navbar'
 import Header from './Components/Header'
 import Welcome from './Components/Welcome'
